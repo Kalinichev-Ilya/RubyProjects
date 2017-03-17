d96d2701cbb823b4238e3659726a1fe160f5623b
Iluya Kalinichev <iluya.kalinichev@gmail.com>
2017-03-17 08:34:40 -0700

    Magic ball

---
+++ Lesson1/MagicBall/8ball.rb
@@ -0,0 +1,32 @@
+# Magic 8 ball emulation
+
+answers = [
+	#Positive
+	"It is certain",
+	"It is decidedly so",
+	"Without a doubt",
+	"Yes — definitely",
+	"You may rely on it",
+
+	#Hesitantly positive
+	"As I see it, yes",
+	"Most likely",
+	"Outlook good",
+	"Signs point to yes",
+	"Yes",
+
+	#Neutral
+	"Reply hazy, try again",
+	"Ask again later",
+	"Better not tell you now",
+	"Cannot predict now",
+	"Concentrate and ask again",
+
+	#Negative
+	"Don’t count on it",
+	"My reply is no",
+	"My sources say no",
+	"Outlook not so good",
+	"Very doubtful"]
+
+puts answers.sample
