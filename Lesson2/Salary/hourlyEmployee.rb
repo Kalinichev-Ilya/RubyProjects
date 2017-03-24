460d3c8fa27a08bab16825d3362e52def2f80ba3
Iluya Kalinichev <iluya.kalinichev@gmail.com>
2017-03-24 07:09:13 -0700

    Hourly employee class implementation

---
+++ Lesson2/Salary/hourlyEmployee.rb
@@ -0,0 +1,44 @@
+#Hourly employees factory method
+require_relative 'employee'
+
+class HourlyEmployee < Employee
+
+	def self.security_guard(name)
+		HourlyEmployee.new(name, 19.25, 30)
+	end
+	def self.cashier(name)
+		HourlyEmployee.new(name, 12.75, 25)
+	end
+	def self.janitor(name)
+		HourlyEmployee.new(name, 10.50, 20)
+	end
+
+	attr_reader :hourly_wage, :hours_per_week
+
+	def hourly_wage=(hourly_wage)
+		if hourly_wage < 0
+			raise "An hourly wage of #{hourly_wage} isn't valid!"
+		end
+		@hourly_wage = hourly_wage
+	end
+
+	def hours_per_week=(hours_per_week)
+		if hours_per_week < 0
+			raise "#{hours_per_week} hourse per week isn't valid!"
+		end
+		@hours_per_week = hours_per_week
+	end
+
+	def initialize(name = "Anonymous", hourly_wage = 0.0, hours_per_week = 0.0)
+		super(name)
+		self.hourly_wage = hourly_wage
+		self.hours_per_week = hours_per_week
+	end
+
+	def print_pay_stub
+		print_name
+		pay_for_period = hourly_wage * hours_per_week * 2
+		formatted_pay = format("$%.2f", pay_for_period)
+		puts "Pay this period: #{formatted_pay}."
+	end
+end
