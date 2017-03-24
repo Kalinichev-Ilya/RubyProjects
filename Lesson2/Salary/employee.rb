d832743197cc1b00eaf9c19df244731b3a8a76b1
Iluya Kalinichev <iluya.kalinichev@gmail.com>
2017-03-24 07:08:03 -0700

    Employee Class implementation

---
+++ Lesson2/Salary/employee.rb
@@ -0,0 +1,20 @@
+#Employee class, contains a name
+#Because name parameter is for all employees
+class Employee
+	attr_reader :name
+
+	def name=(name)
+		if name ==""
+			raise "Name can't be blank!"
+		end
+		@name = name
+	end
+
+	def initialize(name = "Anonymous")
+		self.name = name
+	end
+
+	def print_name
+		puts "Name: #{name}"
+	end
+end
