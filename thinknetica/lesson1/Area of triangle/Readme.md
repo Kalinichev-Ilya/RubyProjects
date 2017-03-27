5e00eb59e6cc5ab06a004c46eab3eae561e31135
Iluya Kalinichev <iluya.kalinichev@gmail.com>
2017-03-27 15:23:58 -0700

    Triangle area

---
+++ thinknetica/lesson1/Area of triangle/Readme.md
@@ -0,0 +1,3 @@
+# Area of a triangle. 
+
+>The area of the triangle can be calculated by knowing its base (a) and height (h) by the formula: ```1/2 * a * h```. The program query the base and height of the triangle and return its area.
---
+++ thinknetica/lesson1/Area of triangle/triangle_area.rb
@@ -0,0 +1,12 @@
+=begin
+	The program query the base and height 
+	of the triangle and return its area.
+=end
+
+print "Input base of triangle: "
+base = gets.chop.to_i
+
+print "Input height of triangle: "
+height = gets.chop.to_i
+
+print "Triangle area is #{0.5*base*height}\n"
