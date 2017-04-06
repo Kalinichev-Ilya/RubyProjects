097805f40c2fff02bbef39f5b91022e8a77f5f2d
Iluya Kalinichev <iluya.kalinichev@gmail.com>
2017-04-06 14:09:28 -0700

    some fixes and code style

--- thinknetica/lesson3/train.rb
+++ thinknetica/lesson3/train.rb
@@ -1,73 +1,61 @@
 require_relative 'route'
 require_relative 'errors'
 
+# ..
 class Train
   attr_writer :speed
-  attr_reader :route
+  attr_accessor :route
 
-  def initialize(wagons = 1, speed = 0)   
+  # @param [Wagon] wagons
+  # @param [String] speed
+  def initialize(wagons = 1, speed = 0, route = [])
     @wagons = wagons
     @speed = speed
-  end
-  
-  #train speed up
-  def speed_up(speed)
-    @speed +=speed
+    @route = route
   end
 
- #prints the current train speed
-  def show(speed)
+  # train speed up
+  # @param [String] speed
+  def speed_up(speed)
+    @speed += speed
+  end
+
+  # prints the current train speed
+  def show_speed
     puts "Current speed: #{@speed}"
   end
 
-  #Turn on the brakes
+  # Turn on the brakes
   def brake
     @speed = 0
   end
 
-  #displays the number of wagons on the screen
+  # displays the number of wagons on the screen
   def count_wagons
     puts "Number of wagons: #{@wagons}"
   end
 
-  #add wagon
+  # add wagon
   def add_wagon
-    if not_move?
-      @wagon += 1
-    else raise ChangeWagonsException ,"You can't add wagon on the speed"
-    end
+    @wagon += 1 if not_move?
   end
 
-  #delete wagon
+  # delete wagon
   def delete_wagon
-    if not_move?
-      @wagon -= 1
-    else
-      raise ChangeWagonsException ,"You can't delete wagon on the speed"
-    end
+    @wagon -= 1 if not_move?
   end
 
-  #add route
-  def route=(route)
-    @route = route
-  end
-
-  #train rode the route
+  # train rode the route
   def go
-    route.each do |station|  
+    route.each do |station|
       puts "Train on #{station}"
       sleep 3
     end
   end
 
-  private 
+  private
 
   def not_move?
-    if self.speed > 0
-      false
-    else
-      true
-    end
+    @speed > 0
   end
-
 end
