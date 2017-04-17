fa2f7a718e564f4f898c6cfeb3c98cb0aff80fcd
Iluya Kalinichev <iluya.kalinichev@gmail.com>
2017-04-17 16:15:41 -0700

    attr accessor with history

--- thinknetica/lesson3/attr_meta.rb
+++ thinknetica/lesson3/attr_meta.rb
@@ -1,13 +1,29 @@
 # My first attempts to use metaprogramming in ruby
 module AttrMeta
+  @cache_val = []
 
-  # Динамически создает геттеры и сеттеры
-  # для любого кол-ва атрибутов,
-  # при этом сеттер сохраняет все значения
+  # Динамически создает геттеры и сеттеры DONE
+  # для любого кол-ва атрибутов, DONE
+  # при этом сеттер сохраняет все значения DONE
   # инстанс-переменной при изменении этого значения.
   # Также должен быть метод <имя_атрибута>_history,
-  # который возвращает массив всех значений данной переменной.
-  def att_accessor_with_history
+  # который возвращает массив всех значений данной переменной. Done
+  def attr_accessor_with_history(*attrs)
+    # getter
+    attrs.each do |attr|
+      define_method(attr) do
+        instance_variable_get("@#{attr}")
+      end
+      # setter
+      define_method("#{attr}=") do |val|
+        @cache_val << val unless @cache_val.pop.equal?(val)
+        instance_variable_set("@#{attr}", val)
+      end
+      # get history of cache values
+      define_method("#{attr}_history") do
+        @cache
+      end
+    end
 
   end
 
