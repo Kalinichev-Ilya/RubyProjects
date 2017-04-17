495a76fdd02f6167b105eedba1af3eec3c61e8c4
Iluya Kalinichev <iluya.kalinichev@gmail.com>
2017-04-17 16:38:45 -0700

    Added strong attr accessor

--- thinknetica/lesson3/attr_meta.rb
+++ thinknetica/lesson3/attr_meta.rb
@@ -2,12 +2,6 @@
 module AttrMeta
   @cache_val = []
 
-  # Динамически создает геттеры и сеттеры DONE
-  # для любого кол-ва атрибутов, DONE
-  # при этом сеттер сохраняет все значения DONE
-  # инстанс-переменной при изменении этого значения.
-  # Также должен быть метод <имя_атрибута>_history,
-  # который возвращает массив всех значений данной переменной. Done
   def attr_accessor_with_history(*attrs)
     # getter
     attrs.each do |attr|
@@ -27,14 +21,19 @@ module AttrMeta
 
   end
 
-  # принимает имя атрибута и его класс.
-  # При этом создается геттер и сеттер для одноименной
-  # инстанс-переменной, но сеттер проверяет тип присваемоего
-  # значения. Если тип отличается от того, который указан
-  # вторым параметром, то выбрасывается исключение.
-  # Если тип совпадает, то значение присваивается.
-  def strong_attr_acessor(attr_name, his_class)
-
+  def strong_attr_accessor(attr_name, his_class)
+    class_is_match = attr_name.is_a?(his_class)
+    # getter
+    define_method(attr_name) do
+      instance_variable_get("@#{attr_name}")
+    end
+    # setter
+    define_method("#{attr_name}=") do |val|
+      if class_is_match
+        instance_variable_set("@#{attr_name}", val)
+      else
+        raise TypeError, 'Invalid type'
+      end
+    end
   end
-
 end
