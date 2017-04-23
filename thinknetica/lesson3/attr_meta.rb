135a233316b81660291235fc2a0f470908b9925b
ilia <iluya.kalinichev@gmail.com>
2017-04-23 04:45:39 -0700

    attribute meta modul

--- thinknetica/lesson3/attr_meta.rb
+++ thinknetica/lesson3/attr_meta.rb
@@ -6,19 +6,24 @@ module AttrMeta
     # getter
     attrs.each do |attr|
       define_method(attr) do
-        instance_variable_get("@#{attr}")
+        instance_variable_get("@#{attr}".to_sym)
       end
+
       # setter
-      define_method("#{attr}=") do |val|
+      define_method("#{attr}=".to_sym) do |val|
         @cache_val << val unless @cache_val.pop.equal?(val)
-        instance_variable_set("@#{attr}", val)
+        unless instance_variable_defined?("#{attr}=".to_sym)
+          instance_variable_set("@#{attr}", val)
+        end
       end
+
       # get history of cache values
       define_method("#{attr}_history") do
-        @cache
+        value = instance_variable_get('@value_history')
+        return [] if value.nil? || value[name].nil?
+        value[name]
       end
     end
-
   end
 
   def strong_attr_accessor(attr_name, his_class)
@@ -29,11 +34,8 @@ module AttrMeta
     end
     # setter
     define_method("#{attr_name}=") do |val|
-      if class_is_match
-        instance_variable_set("@#{attr_name}", val)
-      else
-        raise TypeError, 'Invalid type'
-      end
+      raise TypeError, 'Invalid type' unless class_is_match
+      instance_variable_set("@#{attr_name}", val)
     end
   end
 end
